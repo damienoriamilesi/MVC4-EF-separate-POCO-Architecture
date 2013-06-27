988a88bb3ca635920b329833e482008617612281
damien oria-milesi <lisandora@live.fr>
2013-06-28 01:15:47 +0200

    DAL

---
+++ MyWebSite/MyWebSite.Data/GenericRepository.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace MyWebSite.Data
+{
+    public class GenericRepository<T> where T : class
+    {
+        private readonly MyModelContainer _context;
+        private readonly DbSet<T> _dbSet;
+
+        public GenericRepository(MyModelContainer context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public virtual IEnumerable<T> Get(
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            string includeProperties = "")
+        {
+            IQueryable<T> query = _dbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            foreach (string includeProperty in includeProperties.Split
+                (new[] {','}, StringSplitOptions.RemoveEmptyEntries))
+            {
+                query = query.Include(includeProperty);
+            }
+
+            return orderBy != null ? orderBy(query).ToList() : query.ToList();
+        }
+
+        public virtual T GetByID(object id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public virtual void Insert(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public virtual void Delete(object id)
+        {
+            T entityToDelete = _dbSet.Find(id);
+            Delete(entityToDelete);
+        }
+
+        public virtual void Delete(T entityToDelete)
+        {
+            if (_context.Entry(entityToDelete).State == EntityState.Detached)
+            {
+                _dbSet.Attach(entityToDelete);
+            }
+            _dbSet.Remove(entityToDelete);
+        }
+
+        public virtual void Update(T entityToUpdate)
+        {
+            _dbSet.Attach(entityToUpdate);
+            _context.Entry(entityToUpdate).State = EntityState.Modified;
+        }
+    }
+}
---
+++ MyWebSite/MyWebSite.Data/IReferentialRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyWebSite.Domain;
+
+namespace MyWebSite.Data
+{
+    public interface IReferentialRepository
+    {
+        IList<Entity1> GetAll();
+    }
+
+    public class ReferentialRepository : IReferentialRepository
+    {
+        public IList<Entity1> GetAll()
+        {
+            return new List<Entity1>();
+        }
+    }
+}
