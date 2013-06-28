6fe4f135a0e64edbe492f8fe8ec064f698cce787
damien oria-milesi <lisandora@live.fr>
2013-06-28 22:57:09 +0200

    Delete methods

--- MyWebSite/MyWebSite.Data/GenericRepository.cs
+++ MyWebSite/MyWebSite.Data/GenericRepository.cs
@@ -69,5 +69,21 @@ namespace MyWebSite.Data
             _dbSet.Attach(entityToUpdate);
             _context.Entry(entityToUpdate).State = EntityState.Modified;
         }
+
+        /*public virtual void Delete(object id)
+            {
+                TEntity entityToDelete = dbSet.Find(id);
+                dbSet.Remove(entityToDelete);
+            }
+
+            public virtual void Delete(TEntity entityToDelete)
+            {
+                        if (context.Entry(entityToDelete).State == EntityState.Detached)
+                        {
+                            dbSet.Attach(entityToDelete);
+                        }
+                        dbSet.Remove(entityToDelete);
+
+            }*/
     }
 }
